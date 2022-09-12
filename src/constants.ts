6d6292ba19056d2d96bee1f4079e8f8b09dda28b
christopher.millward <christopher.millward@cloudkitchens.com>
2022-09-11 22:14:30 -0400

    Tweaked constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const DAYS_IN_YEAR = 365;
 export const HOURS_IN_DAY = 24;
 
-export const FATIGUE_PER_HOUR = 0.01;
-export const HEALTH_PER_FAMISHED_HOUR = 0.01;
-export const HUNGER_PER_HOUR = 0.01;
+export const FATIGUE_PER_HOUR = 0.1;
+export const HEALTH_PER_FAMISHED_HOUR = 0.02;
+export const HUNGER_PER_HOUR = 0.1;
